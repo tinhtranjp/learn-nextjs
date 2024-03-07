411285f9e5b66df35e8f1aea3e273eac7f4098c9
tinhtranjp <trandinhtinh9999@gmail.com>
2024-03-07 22:42:48 +0900

    learn nextjs

---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { Suspense } from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from '@nextui-org/react'
+import HeaderAuth from './header-auth'
+import SearchInput from './search-input'
+
+export default function Header() {
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Suspense>
+            <SearchInput />
+          </Suspense>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
+    </Navbar>
+  )
+}
